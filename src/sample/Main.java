cbd1cee84d34057d8c30cc7e6adda415433eb9a1
Rus1994 <mirzoew.rustam@gmail.com>
2021-04-11 13:37:40 +0400

    Лого+

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.io.File;
 
 public class Main extends Application {
 
@@ -13,7 +18,13 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setTitle("Смертельная битва");
+        primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("MK_Logo.png")));
         primaryStage.show();
+
+        String sound = "MK.mp3";
+        Media hit = new Media(new File(sound).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(hit);
+        mediaPlayer.play();
     }
 
 
